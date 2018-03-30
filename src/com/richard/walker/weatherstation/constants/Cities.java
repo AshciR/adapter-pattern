eb35d0798eb695accf3523d236f11d778af216f0
Nicholas Biddell <nicbiddell@gmail.com>
2018-03-30 16:52:57 -0400

    Add Generator to create weather data

---
+++ src/com/richard/walker/weatherstation/constants/Cities.java
@@ -0,0 +1,10 @@
+package com.richard.walker.weatherstation.constants;
+
+public final class Cities {
+    public final static  String[] CITIES = new String[]
+            {"Raleigh", "Durham", "Chapel Hill", "Charlotte", "Atlanta", "Miami", "Orlando", "New York", "Boston", "Washington D.C."};
+
+    public Cities() {
+        throw new UnsupportedOperationException("Constructor not implemented");
+    }
+}
---
+++ src/com/richard/walker/weatherstation/utilities/WeatherGenerator.java
@@ -0,0 +1,38 @@
+package com.richard.walker.weatherstation.utilities;
+
+import com.richard.walker.weatherstation.constants.Cities;
+import com.richard.walker.weatherstation.domain.WeatherStation;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class WeatherGenerator {
+    private static int MAX_TEMP = 110;
+    private static int MIN_TEMP = 20;
+    private static int MAX_WIND_SPEED = 50;
+    private static int MAX_HUMIDITY = 100;
+
+    public static WeatherStation[] generateMockWeatherData() {
+        int numCities = Cities.CITIES.length;
+
+        WeatherStation[] weatherStations = new WeatherStation[numCities];
+        ThreadLocalRandom rand = ThreadLocalRandom.current();
+
+
+
+        for (int i = 0; i < numCities; i++) {
+            int temp = rand.nextInt(MIN_TEMP, MAX_TEMP);
+            int windSpeed = rand.nextInt(MAX_WIND_SPEED);
+            int humidity = rand.nextInt(MAX_HUMIDITY);
+            String city = Cities.CITIES[i];
+
+            WeatherStation station = new WeatherStation(temp, windSpeed, humidity, city);
+            weatherStations[i] = station;
+        }
+
+        return weatherStations;
+    }
+
+    public WeatherGenerator() {
+        throw new UnsupportedOperationException("Constructor not implemented");
+    }
+}
